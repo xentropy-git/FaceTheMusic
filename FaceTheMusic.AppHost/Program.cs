f5c78099e7e580a693cfda0cb5c3388da37d2db6
xentropy <claponsie@gmail.com>
2025-05-17 10:46:17 -0400

    Refactored for central version control

--- FaceTheMusic.AppHost/Program.cs
+++ FaceTheMusic.AppHost/Program.cs
@@ -1,6 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.FaceTheMusic_Api>("api");
+var sql = builder.AddSqlServer("sql")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var db = sql.AddDatabase("db");
+
+var apiService = builder.AddProject<Projects.FaceTheMusic_Api>("api")
+    .WithReference(db)
+    .WaitFor(db);
 /*
 builder.AddProject<Projects.aspire_test_Web>("webfrontend")
     .WithExternalHttpEndpoints()
